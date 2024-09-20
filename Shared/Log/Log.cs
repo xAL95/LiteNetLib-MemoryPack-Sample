860af809d8888af54bb4b83069eb6ec99a015364
xAL95 <111229021+xAL95@users.noreply.github.com>
2024-09-20 03:03:44 +0200

    Fix log

---
+++ Shared/Log/Log.cs
@@ -0,0 +1,25 @@
+﻿using LiteNetLib;
+
+public class Log : INetLogger
+{
+    public static void Write(string str)
+    {
+        Console.WriteLine(str);
+    }
+
+    void INetLogger.WriteNet(NetLogLevel level, string str, params object[] args)
+	{
+		switch (level)
+		{
+			case NetLogLevel.Warning:
+			case NetLogLevel.Error:
+			case NetLogLevel.Trace:
+			case NetLogLevel.Info:
+				{
+					Console.WriteLine(str, args);
+				}
+				break;
+		}
+	}
+}
+
